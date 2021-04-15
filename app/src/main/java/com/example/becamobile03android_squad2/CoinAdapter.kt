2eda77bcd4bc0829a5c46adf7f4a9d2ce8a6afe3
AndreAliardi <andre.aliardi@gmail.com>
2021-04-15 11:30:18 -0300

    Criando classe CoinAdapter no Adapter e sobreescrevendo os metodos

--- app/src/main/java/com/example/becamobile03android_squad2/CoinAdapter.kt
+++ app/src/main/java/com/example/becamobile03android_squad2/CoinAdapter.kt
@@ -1,7 +1,24 @@
 package com.example.becamobile03android_squad2
 
 import android.view.View
+import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+
+class CoinAdapter(private val list: List<Coin>, private val listener: MainActivity) :
+    RecyclerView.Adapter<CoinAdapter.CoinViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CoinAdapter.CoinViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: CoinAdapter.CoinViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+}
 
 class CoinViewHolder(
     itemView: View,
