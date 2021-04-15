de2c477052e1d47f3cc291add786eec2e8e9d777
Kauane <kauane.nep@gmail.com>
2021-04-15 10:35:36 -0300

    Criação interface Api

---
+++ app/src/main/java/com/example/becamobile03android_squad2/CoinApi.kt
@@ -0,0 +1,10 @@
+package com.example.becamobile03android_squad2
+
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface CoinApi {
+
+    @GET("v1/assets?")
+    fun getAllList(@Query("apiKey") api:String): retrofit2.Call<List<>>
+}
