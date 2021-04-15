6ae9051a1a52416143696d0bc4fa45a76848bd8b
Kauane <kauane.nep@gmail.com>
2021-04-15 11:58:31 -0300

    Criação da class para set do adapter e contexto da main

--- app/src/main/java/com/example/becamobile03android_squad2/MainActivity.kt
+++ app/src/main/java/com/example/becamobile03android_squad2/MainActivity.kt
@@ -2,10 +2,28 @@ package com.example.becamobile03android_squad2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.viewModels
+import androidx.recyclerview.widget.GridLayoutManager
+import kotlinx.android.synthetic.main.layout_tela_inicial.*
+import java.util.Observer
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setContentView(R.layout.layout_tela_inicial)
+        val viewModel: CoinViewModel by viewModels()
+        viewModel.init()
+        viewModel.listCoin.observe(this, Observer {
+            setAdapeter(it)
+        }
+
+        )
     }
+
+    private fun setAdapeter(coin: List<Coin>?) {
+        moeda.layoutManager = GridLayoutManager(this@MainActivity, 1)
+        moeda.adapter = coin?.let { CoinAdapter(coin, this) }
+    }
+
 }
