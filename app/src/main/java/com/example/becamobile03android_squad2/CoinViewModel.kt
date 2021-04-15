bf985b61af434a5f3d5e858f65048f89d02a22d3
Kauane <kauane.nep@gmail.com>
2021-04-15 11:56:21 -0300

    Criação class que sera observada na main

--- app/src/main/java/com/example/becamobile03android_squad2/CoinViewModel.kt
+++ app/src/main/java/com/example/becamobile03android_squad2/CoinViewModel.kt
@@ -3,6 +3,8 @@ package com.example.becamobile03android_squad2
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import retrofit2.Call
+import retrofit2.Response
 
 class CoinViewModel : ViewModel() {
 
@@ -10,4 +12,25 @@ class CoinViewModel : ViewModel() {
     private val coinLiveData: MutableLiveData<List<Coin>> = MutableLiveData()
     val listCoin: LiveData<List<Coin>>
         get() = coinLiveData
+
+
+    private fun callListCoin() {
+        val call = CoinService.coinRetrofitApi().getAllList("CFB9107C-F454-4F93-B412-C7F15E3D284D")
+        call.enqueue(object : retrofit2.Callback<List<Coin>> {
+            override fun onResponse(call: Call<List<Coin>>, response: Response<List<Coin>>) {
+                if (response.isSuccessful)
+                    response.body()?.forEach {
+                        listCoinResult.add(it)
+                    }
+                coinLiveData.postValue(listCoinResult)
+            }
+
+            override fun onFailure(call: Call<List<Coin>>, t: Throwable) {
+                coinLiveData.postValue(null)
+            }
+        })
+
+    }
+
 }
+
