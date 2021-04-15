3b4593b3b528389613d1efce36beb8ff031388cd
luana-barbosa <luana_barbosa93@hotmail.com>
2021-04-15 19:17:51 -0300

    Config

--- app/src/main/java/com/example/becamobile03android_squad2/view/DetailsCoin.kt
+++ app/src/main/java/com/example/becamobile03android_squad2/view/DetailsCoin.kt
@@ -1,12 +1,13 @@
 package com.example.becamobile03android_squad2.view
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import com.example.becamobile03android_squad2.R
 
 class DetailsCoin : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_details_coin)
+
     }
-}
