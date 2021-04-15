ab1d0b43ef6949e675288811478f8981e7987741
Kauane <kauane.nep@gmail.com>
2021-04-15 11:54:55 -0300

    Criação variavel LiveData e recebendo uma lista de Coin

--- app/src/main/java/com/example/becamobile03android_squad2/CoinViewModel.kt
+++ app/src/main/java/com/example/becamobile03android_squad2/CoinViewModel.kt
@@ -1,6 +1,13 @@
 package com.example.becamobile03android_squad2
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class CoinViewModel : ViewModel() {
+
+    val listCoinResult: MutableList<Coin> = arrayListOf()
+    private val coinLiveData: MutableLiveData<List<Coin>> = MutableLiveData()
+    val listCoin: LiveData<List<Coin>>
+        get() = coinLiveData
 }
