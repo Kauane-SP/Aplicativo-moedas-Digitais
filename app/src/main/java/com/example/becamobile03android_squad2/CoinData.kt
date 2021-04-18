962fd8acb7458bf694296bd8b00e60d500bd2937
Kauane <kauane.nep@gmail.com>
2021-04-18 13:25:50 -0300

    Criação de classe chamada da data no layout do aplicativo

---
+++ app/src/main/java/com/example/becamobile03android_squad2/CoinData.kt
@@ -0,0 +1,17 @@
+package com.example.becamobile03android_squad2
+
+import android.os.Build
+import androidx.annotation.RequiresApi
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+
+class CoinData {
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    fun callDate() : String{
+        val data =  LocalDateTime.now()
+        val style = DateTimeFormatter.ofPattern("dd MMM yyyy")
+        return data.format(style).toString()
+    }
+
+}
