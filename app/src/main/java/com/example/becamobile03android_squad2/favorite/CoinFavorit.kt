e98b46654e15796652b864c88b26cd22d355a222
luana-barbosa <luana_barbosa93@hotmail.com>
2021-04-19 12:51:25 -0300

    Correcao

--- app/src/main/java/com/example/becamobile03android_squad2/favorite/CoinFavorit.kt
+++ app/src/main/java/com/example/becamobile03android_squad2/favorite/CoinFavorit.kt
@@ -4,9 +4,9 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.becamobile03android_squad2.R
 
-class CoinFavorit : AppCompatActivity() {
+class CoinFavorite : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_coin_favorit)
+        setContentView(R.layout.activity_coin_favorite)
     }
 }
